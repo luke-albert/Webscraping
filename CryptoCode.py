6b32f9c7c76b99659c15843ea638cc3e068a7ecf
Luke <luke_albert1@baylor.edu>
2023-05-04 16:23:27 -0500

    changes

--- CryptoCode.py
+++ CryptoCode.py
@@ -23,10 +23,6 @@ soup = BeautifulSoup(webpage, 'html.parser')
 print(soup.title.text)
 
 table_rows = soup.findAll("tr")
-# print(len(table_rows))
-# print(table_rows[2:6])
-# element = soup.find('td', class_="filter-item")
-# print(element)
 
 
 # putting webscraped data into excel
@@ -38,26 +34,30 @@ ws = wb.active
 ws.title = 'Crypto Tracker'
 
 ws['A1'] = 'Symbol'
-ws['A1'].font = Font(name='Times New Roman', size=12, italic=False, bold=True)
-ws.column_dimensions['A'].width = 10
+ws['A1'].font = Font(name='Times New Roman',
+                     size=14, italic=False, bold=True)
+ws.column_dimensions['A'].width = 12
 
 ws['B1'] = 'Name'
-ws['B1'].font = Font(name='Times New Roman', size=12, italic=False, bold=True)
-ws.column_dimensions['B'].width = 10
+ws['B1'].font = Font(name='Times New Roman',
+                     size=14, italic=False, bold=True)
+ws.column_dimensions['B'].width = 22
 
 ws['C1'] = 'Current Price'
-ws['C1'].font = Font(name='Times New Roman', size=12, italic=False, bold=True)
+ws['C1'].font = Font(name='Times New Roman',
+                     size=14, italic=False, bold=True)
 ws.column_dimensions['C'].width = 20
 
 ws['D1'] = 'Percent Change'
-ws['D1'].font = Font(name='Times New Roman', size=12,
+ws['D1'].font = Font(name='Times New Roman', size=14,
                      italic=False, bold=True)
 
-ws.column_dimensions['D'].width = 20
+ws.column_dimensions['D'].width = 22
 
 ws['E1'] = "Yesterday's Price"
-ws['E1'].font = Font(name='Times New Roman', size=12, italic=False, bold=True)
-ws.column_dimensions['E'].width = 20
+ws['E1'].font = Font(name='Times New Roman',
+                     size=14, italic=False, bold=True)
+ws.column_dimensions['E'].width = 22
 
 write_sheet = wb['Crypto Tracker']
 
@@ -66,8 +66,12 @@ i = 2
 for row in table_rows[1:6]:
     td = list(row.findAll("td"))
     symbol_and_name_list = td[2].text.split()
-    name = symbol_and_name_list[0]
-    symbol = symbol_and_name_list[1]
+
+    if len(symbol_and_name_list) == 3:
+        name = symbol_and_name_list[0] + ' ' + symbol_and_name_list[1]
+    else:
+        name = symbol_and_name_list[0]
+    symbol = symbol_and_name_list[-1]
     current_price = td[3].text
     percent_change = td[5].text
 
@@ -81,14 +85,18 @@ for row in table_rows[1:6]:
         current_price_without_dollarsign) / (1 + number_as_percent)
     round_yesterday_price = round(yesterday_price, 4)
 
+    grey_font = Font(color='838B8B')
     write_sheet.cell(i, 1).value = symbol
+    write_sheet.cell(i, 1).font = grey_font
     write_sheet.cell(i, 2).value = name
+    write_sheet.cell(i, 2).font = grey_font
     write_sheet.cell(i, 3).value = '$' + str(round_current_price)
-    font = Font(color="008000")
+    write_sheet.cell(i, 3).font = grey_font
+    green_font = Font(color="00C957")
     write_sheet.cell(i, 4).value = percent_change
-    write_sheet.cell(i, 4).font = font
-    # percent_change.font = font
+    write_sheet.cell(i, 4).font = green_font
     write_sheet.cell(i, 5).value = '$' + str(round_yesterday_price)
+    write_sheet.cell(i, 5).font = grey_font
     i += 1
 
     for row in ws.iter_rows(min_row=2, max_row=6, min_col=5, max_col=5):
@@ -130,14 +138,15 @@ for row in table_rows[1:6]:
     yesterday_price = float(
         current_price_without_dollarsign) / (1 + number_as_percent)
 
-    locale.setlocale(locale.LC_ALL, '')  # set the locale to the user's default
+    locale.setlocale(locale.LC_ALL, '')  # formatting currency
     currency_price = locale.currency(yesterday_price, grouping=True)
 
     if name == 'Bitcoin':
         if abs(float(current_price_without_dollarsign) - float(yesterday_price)) > 5:
             client = Client(keys.account_sid, keys.auth_token)
             message = name + ' has changed to a price of ' + \
-                str(current_price) + ' from ' + currency_price
+                str(current_price) + \
+                " from yesterday's price of " + currency_price
             TWnumber = '+12147616829'
             myphone = '+12817506263'
             textmsg = client.messages.create(
@@ -148,7 +157,8 @@ for row in table_rows[1:6]:
         if abs(float(current_price_without_dollarsign) - float(yesterday_price)) > 5:
             client = Client(keys.account_sid, keys.auth_token)
             message = name + ' has changed to a price of ' + \
-                str(current_price) + ' from ' + currency_price
+                str(current_price) + \
+                " from yesterday's price of " + currency_price
             TWnumber = '+12147616829'
             myphone = '+12817506263'
             textmsg = client.messages.create(
